b9c579d31d3af20a6da024291ae78d0a56dd3c6b
Majorena <marcela.eneroth@gmail.com>
2017-09-27 16:40:16 +0200

    Ändrat i quiz och frågor

--- quiz/views.py
+++ quiz/views.py
@@ -25,11 +25,24 @@ def startpage(request):
 		"quizzes": quizzes,
 	}
 	return render(request, "start.html", context)
+
 def quiz(request, quiz_number):
-	return render(request, "quiz.html")
+	context = {
+		"quiz": quizzes[quiz_number - 1],
+		"quiz_number": quiz_number,
+	}
+	return render(request, "quiz.html", context)
 
 def question(request, quiz_number, question_number):
-	return render(request, "question.html")
+	context = {
+		"question_number": question_number,
+	    	"question": "Hur många bultar har ölandsbron?",
+		"answer1": "12",
+	   	"answer2": "66 400",
+	    	"answer3": "7 428 954",
+	    	"quiz_number": quiz_number,
+	}
+	return render(request, "question.html", context)
 
 def completed(request, quiz_number):
 	return render(request, "completed.html")
