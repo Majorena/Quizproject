a9c2bb601a7d5343baf40638bbe49f3489e67c5c
Majorena <marcela.eneroth@gmail.com>
2017-09-28 10:50:56 +0200

    börjar koppla django till databasen

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -19,6 +19,8 @@ Including another URLconf
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 # ]
+
+from django.contrib import admin
 from django.urls import path
 from quiz import views
 
@@ -28,4 +30,4 @@ path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),	
 ]
-
+path('admin/', admin.site.urls),
