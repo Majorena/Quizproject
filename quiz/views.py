acf31cc465767b5d2040f8c3006685562d4665b6
Majorena <marcela.eneroth@gmail.com>
2017-09-28 12:04:42 +0200

    fixar frågor i admin

--- quiz/views.py
+++ quiz/views.py
@@ -1,36 +1,19 @@
 from django.shortcuts import render
 
-quizzes = [
-	{
-		"quiz_number": 1,
-		"name": "Klassiska böcker",
-		"description": "Hur bra kan du dina klassiker?"
-	},
-	{
-		"quiz_number": 2,
-		"name": "Största fotbollslagen",
-		"description": "Kan du dina lag?"
-	},
-	{
-		"quiz_number": 3,
-		"name": "Världens mest kända hackare",
-		"description": "Kan du din hackerhistoria?"
-	},
-]
-
-# def startpage(request):
-# 	return render(request, "start.html")
+from quiz.models import Quiz
 def startpage(request):
 	context = {
-		"quizzes": quizzes,
+	    	"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "start.html", context)
 
+
 def quiz(request, quiz_number):
 	context = {
-		"quiz": quizzes[quiz_number - 1],
+	    	"quiz": Quiz.objects.get(quiz_number=quiz_number),
 		"quiz_number": quiz_number,
 	}
+	
 	return render(request, "quiz.html", context)
 
 def question(request, quiz_number, question_number):
