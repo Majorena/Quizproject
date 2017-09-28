7960d9e8fa1ab91bf2a338d6f8aedf98c4ed9113
Majorena <marcela.eneroth@gmail.com>
2017-09-28 11:00:31 +0200

    skapade django admin

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -29,5 +29,5 @@ path("", views.startpage, name="start_page"),
 path("quiz/<int:quiz_number>/", views.quiz, name="quiz_page"),
 path("quiz/<int:quiz_number>/question/<int:question_number>/", views.question, name="question_page"),
 path("quiz/<int:quiz_number>/completed/", views.completed, name="completed_page"),	
+path('admin/', admin.site.urls), 
 ]
-path('admin/', admin.site.urls),
